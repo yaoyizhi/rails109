c399fb94d2d4130f5d155e12ef4d01a41e29bb29
yaoyizhi <email@yaoadou888>
2017-04-05 12:03:22 +0800

    implement movie action

---
+++ rails109/app/controllers/movies_controller.rb
@@ -0,0 +1,50 @@
+class MoviesController < ApplicationController
+    before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+
+    def index
+        @movies = Movie.all
+    end
+
+    def show
+        @movie = Movie.find(params[:id])
+    end
+
+    def new
+        @movie = Movie.new
+    end
+
+    def create
+        @movie = Movie.new(movie_params)
+        if @movie.save
+            redirect_to movies_path
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @movie = Movie.find(params[:id])
+    end
+
+    def update
+        @movie = Movie.find(params[:id])
+        if @movie.update(movie_params)
+            redirect_to movies_path
+            flash[:notice] = 'Update Success'
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @movie = Movie.find(params[:id])
+        @movie.destroy
+        redirect_to movies_path
+    end
+
+    private
+
+    def movie_params
+        params.require(:movie).permit(:title, :description)
+    end
+end
